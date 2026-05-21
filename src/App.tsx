9eeb97e32f3ed49dbc29c47fc7d7ef15b77347d5
Avishkar Bhandare <avi.designwings@gmail.com>
2026-05-22 01:01:02 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,23 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import Home from './pages/Home';
+import ProjectDetail from './pages/ProjectDetail';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project/:id" element={<ProjectDetail />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
